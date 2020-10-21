a7b2746eaf9eaef30419ec636169bf635c0a178f
idk why i aint on vacation <57840931+NoNameSpecified@users.noreply.github.com>
2020-10-21 23:12:50 +0200

    Add files via upload

---
+++ src/includes/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="DE, de">
+    <head>
+        <!-- https://www.w3schools.com/tags/tag_meta.asp -->
+        <meta charset="UTF-8">
+        <meta name="description" content="Der Hüpfburg Discord Server">
+        <meta name="keywords" content="hüpfburg, discord, server">
+        <meta name="author" content="someguy">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- page display info & styling -->
+        <title>Die Hüpfburg !</title>
+        <link rel="stylesheet" type="text/css" href="/huepfburg/style/style.css">
+        <link rel="shortcut icon" type="image/x-icon" href="/huepfburg/img/icon/hburg_icon.ico">
+        <script src="/huepfburg/scripts/script.js"></script> 
+    </head>
+
+    <!-- script temporarly stored here -->
+    <script>
+        resizeNavOnScroll();
+    </script>
+
+    <!-- header will be shown on top with nav etc -->
+
+    <header id="main_header">
+        <nav id="main_nav">
+            <div class="nav_title"><h3>Die Hüpfburg</h3></div>
+            <ul>
+                <!-- removed the title="title" -->
+
+                <li><a href="/huepfburg/index.php#" class="current_active">Home</a></li>
+                <!-- Channels, Themenchannels, Ränge, ... -->
+                <li><a href="/huepfburg/index.php#aufbau" class="current_active">Aufbau</a></li>
+                <!-- Entstehung, Geschichte bis jetzt -->
+                <li><a href="/huepfburg/index.php#geschichte" class="current_active">Geschichte</a></li>
+                <!-- Wie der Server moderiert wird (2 Owner usw) -->
+                <li><a href="/huepfburg/index.php#moderation" class="current_active">Moderation</a></li>
+                <!-- Link zum join-button, der aber immer highlighted sein wird-->
+                <li class="highlight"><a href="/huepfburg/index.php#join">Join</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <body>
+        
+    </body>
+    <!--
+        Rest of the Code in the index.php
+    -->
